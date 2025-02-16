18ec480f45384c7b19f031f5398e45ea09b6681c
Fidel Alejandro Cepero Salazar <fidelsalazar990923@gmail.com>
2025-02-16 15:13:52 -0500

    first commit

---
+++ src/data/socials.ts
@@ -0,0 +1,13 @@
+export const tag = "@gonzalez-daniela";
+export const author = "Daniela Gonzalez Dominguez";
+
+export const defaultMeta = {
+  title: "Daniela Gonzalez - Home",
+  description:
+    "Discover the portfolio and blog of Daniela Gonzalez, a  QA analist from Cuba. Explore his expertise in testing, programming languages, and more.",
+  keywords: ["daniela gonzalez", "junior qa analist", "junior", "programming", "portfolio", "blog", "cuba"],
+  ogImage: {
+    src: "/banner.png",
+    alt: "Daniela Gonzalez - Home",
+  },
+};
